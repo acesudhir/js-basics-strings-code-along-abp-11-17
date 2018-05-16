c85ec3ed9c94c989a1963ff1536248cd7bbdad83
Sudhir Menon <sudhirhpp@gmail.com>
2018-05-16 15:02:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let currentUser = "Sudhir Menon";
 
 let welcomeMessage = "Welcome to Flatbook, "+currentUser+"!";
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+currentUser+"!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
